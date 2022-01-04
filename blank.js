35761e3fec5fb1f92e2885394023e70de0c781ff
kingkong1106 <78448474+kingkong1106@users.noreply.github.com>
2022-01-04 10:53:28 -0500

    Add files via upload

---
+++ blank.js
@@ -0,0 +1,37 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format:'png',
+    png_quality: 90
+
+}
+);
+camera= document.getElementById('camera');
+Webcam.attach('#camera');
+function take_a_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="' + data_uri + '">';
+
+    });
+}
+console.log('ml5 version : ',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json', modelloaded);
+function modelloaded()
+{
+    console.log('model loaded')
+}
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1 = "the first prediction is" + prediction_1;
+    speak_data_2 = "and the second prediction is" + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+function check()
+{
+    img = document.getElementById('capture_image');
+    classifier.classify(img, gotResult);
+
+}
